a3543b4909fb8f05bbcf3ff8a4968cd6a254e6c5
helenaMGV <a01254777@tec.mx>
2025-11-26 20:39:30 -0700

    Add exponentiation to main.cpp
    Agregación de la función de exponenciación a main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "operations.h"
+#include <cmath>
+
 using namespace std;
 int main() {
 int option;
 double a, b;
 cout << "Mini Calculadora Colaborativa\n";
-cout << "1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n";
+cout << "1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n5. Exponenciar a^b\n";
 cout << "Elige una opción: ";
 cin >> option;
 cout << "Ingresa dos números: ";
@@ -15,6 +17,7 @@ case 1: cout << "Resultado: " << add(a, b); break;
 case 2: cout << "Resultado: " << subtract(a, b); break;
 case 3: cout << "Resultado: " << multiply(a, b); break;
 case 4: cout << "Resultado: " << divide(a, b); break;
+case 5: cout << "Resultado: " << power(a,b); break;
 default: cout << "Opción inválida";
 }
 cout << endl;
